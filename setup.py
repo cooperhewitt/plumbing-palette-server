70bf6ad749dfc7825c037d03ab36af077d8c7e35
straup <straup@gmail.com>
2014-11-13 15:06:29 +0100

    setup wrangling

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-# THIS DOES NOT WORK YET
-
 from setuptools import setup
 
 setup(name='plumbing-palette-server',
@@ -10,17 +8,14 @@ setup(name='plumbing-palette-server',
       author='Cooper Hewitt Smithsonian Design Museum',
       url='https://github.com/cooperhewitt/plumbing-palette-server',
       requires=[
-          'flask',
-          # 'flask-cors',
-          'roygbiv',
       ],
       dependency_links=[
-          'https://github.com/cooperhewitt/py-cooperhewitt-flask/tarball/master#egg=cooperhewitt.flask-0.1',
-          'https://github.com/cooperhewitt/py-cooperhewitt-swatchbook/tarball/master#egg=cooperhewitt.swatchbook-0.2',
+          'https://github.com/cooperhewitt/py-cooperhewitt-flask/tarball/master#egg=cooperhewitt.flask-0.3',
+          'https://github.com/cooperhewitt/py-cooperhewitt-swatchbook/tarball/master#egg=cooperhewitt.swatchbook-0.3',
       ],
       install_requires=[
-          'cooperhewitt.flask>=0.1',
-          'cooperhewitt.swatchbook>=0.1',
+          'cooperhewitt.flask',
+          'cooperhewitt.swatchbook',
       ],
       packages=[],
       scripts=[
