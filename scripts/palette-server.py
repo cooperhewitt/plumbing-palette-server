84c1bbfe7574c6c1089a6c7fa84b8c317796d478
straup <straup@gmail.com>
2014-11-17 14:50:52 +0100

    run from cli

--- scripts/palette-server.py
+++ scripts/palette-server.py
@@ -11,9 +11,6 @@ import roygbiv
 import cooperhewitt.swatchbook as swatchbook
 import cooperhewitt.flask.http_pony as http_pony
 
-# This replaces the normal
-# 'app = flask.Flask(__name__)' dance
-
 app = http_pony.setup_flask_app('PALETTE_SERVER')
 
 @app.route('/ping', methods=['GET'])
@@ -78,26 +75,4 @@ def _extract(path, reference):
     
 if __name__ == '__main__':
 
-    import sys
-    import optparse
-    import ConfigParser
-
-    parser = optparse.OptionParser()
-
-    parser.add_option("-c", "--config", dest="config", help="", action="store", default=None)
-    parser.add_option("-v", "--verbose", dest="verbose", help="enable chatty logging; default is false", action="store_true", default=False)
-
-    opts, args = parser.parse_args()
-
-    if opts.verbose:
-        logging.basicConfig(level=logging.DEBUG)
-        logging.debug("verbose logging is enabled")
-    else:
-        logging.basicConfig(level=logging.INFO)
-
-    cfg = http_pony.update_app_config_from_file(app, opts.config)
-
-    port = cfg.get('flask', 'port')
-    port = int(port)
-
-    app.run(port=port)
+    http_pony.run_from_cli(app)
